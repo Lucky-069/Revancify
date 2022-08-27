bab57b9e07368bac67e35cce746e79fb9685e4ff
decipher <84389932+decipher3114@users.noreply.github.com>
2022-08-27 16:28:59 +0530

    Corrected Number for twitter

--- revancify.sh
+++ revancify.sh
@@ -152,7 +152,7 @@ user_input()
     echo "Which app do you want to patch?"
     echo "1. YouTube"
     echo "2. YouTube Music"
-    echo "2. Twitter"
+    echo "3. Twitter"
     read -p "Your Input: " input
     if [ "$input" -eq "1" ]
     then
