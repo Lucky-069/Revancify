4b5587ae835fa4c229573b057798b273c91e027d
decipher <84389932+decipher3114@users.noreply.github.com>
2022-08-30 08:09:42 +0530

    Update revancify.sh

--- revancify.sh
+++ revancify.sh
@@ -550,8 +550,23 @@ reddit_dl()
     fi
 }
 
-
-if [ "$appname" = "YouTube" ]
+if [ "$appname" = "MicroG" ]
+then
+    python fetch.py yt non_root & pid=$!
+    trap "kill $pid 2> /dev/null" EXIT
+    while kill -0 $pid 2> /dev/null; do
+        anim
+    done
+    trap - EXIT
+    sleep 1
+    tput rc; tput cd
+    microglink="$(sed -n '6p' latest.txt)"
+    wget -q -c $microglink -O "Vanced_MicroG.apk" --show-progress --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+    mv "Vanced_MicroG.apk" /storage/emulated/0/Revancify
+    echo MicroG App saved to Revancify folder.
+    termux-open /storage/emulated/0/Revancify/Vanced_MicroG.apk
+    user_input
+elif [ "$appname" = "YouTube" ]
 then
     if [ "$variant" = "root" ]
     then
@@ -739,9 +754,12 @@ then
         anim
     done
     trap - EXIT
+    sleep 1
+    tput rc; tput cd
     microglink="$(sed -n '6p' latest.txt)"
     wget -q -c $microglink -O "Vanced_MicroG.apk" --show-progress --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
     mv "Vanced_MicroG.apk" /storage/emulated/0/Revancify
+    echo MicroG App saved to Revancify folder.
     termux-open /storage/emulated/0/Revancify/Vanced_MicroG.apk
 fi
 cd ~
