1d15f779f4d2f752001c41885355e914ff906b96
decipher <84389932+decipher3114@users.noreply.github.com>
2022-09-04 11:44:44 +0530

    removed unnecessary code

--- fetch.py
+++ fetch.py
@@ -14,8 +14,6 @@ requests_session = requests.Session()
 patches_version = (((requests_session.get('https://api.github.com/repos/revanced/revanced-patches/releases/latest')).json())['name']).replace("v","")
 cli_version = (((requests_session.get('https://api.github.com/repos/revanced/revanced-cli/releases/latest')).json())['name']).replace("v","")
 integrations_version = (((requests_session.get('https://api.github.com/repos/revanced/revanced-integrations/releases/latest')).json())['name']).replace("v","")
-with open("latest.txt", "w") as f:
-    f.write('\n'.join([patches_version, cli_version, integrations_version]))
 
 
 
