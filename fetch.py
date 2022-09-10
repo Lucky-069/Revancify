a644da0c0eaf0ff6e92be0ea4310adcf7a63f164
decipher3114 <shauryasiwach26@gmail.com>
2022-09-10 13:42:24 +0530

    remove unnecessary loop

--- fetch.py
+++ fetch.py
@@ -61,10 +61,8 @@ def yt_root():
 # YouTube patches
 def yt_patches():
     open("youtube_patches.txt", "w").close()
-    a = 0
     for i in (requests.get('https://raw.githubusercontent.com/revanced/revanced-patches/main/patches.json')).json():
         if (((i['compatiblePackages'])[0])['name']) == "com.google.android.youtube" and i['deprecated'] != True:
-            a += 1
             with open("youtube_patches.txt", "a") as p:
                 p.write(str(i['name']) + " " + "on" + "\n")
 
@@ -155,10 +153,8 @@ def ytm_root():
 
 def ytm_patches():
     open("youtubemusic_patches.txt", "w").close()
-    a = 0
     for i in (requests.get('https://raw.githubusercontent.com/revanced/revanced-patches/main/patches.json')).json():
         if (((i['compatiblePackages'])[0])['name']) == "com.google.android.apps.youtube.music" and i['deprecated'] != True:
-            a += 1
             with open("youtubemusic_patches.txt", "a") as p:
                 p.write(str(i['name']) + " " + "on" + "\n")
 
