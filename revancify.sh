f7ba65779fc95346c6a252fb96e0a5a32be1eb64
decipher <84389932+decipher3114@users.noreply.github.com>
2022-09-12 14:53:48 +0530

    fixed clr screen before mount

--- revancify.sh
+++ revancify.sh
@@ -523,8 +523,7 @@ then
         app_dl YouTube "$appver" "$getlink" &&
         echo "Building Youtube Revanced ..."
         java -jar ./revanced-cli*.jar -b ./revanced-patches*.jar -m ./revanced-integrations*.apk -c -a ./YouTube-* $includeyt --keystore ./revanced.keystore -o ./com.google.android.youtube.apk --custom-aapt2-binary ./aapt2 --experimental --options options.toml --exclusive
-        echo "Mounting the apk"
-        sleep 1; tput rc; tput cd
+        echo "Mounting the app"
         if su -mm -c 'stockapp=$(pm path com.google.android.youtube | grep base | sed 's/package://g' ); grep com.google.android.youtube /proc/mounts | while read -r line; do echo $line | cut -d " " -f 2 | xargs -r umount -l; done && mv com.google.android.youtube.apk /data/adb/revanced && revancedapp=/data/adb/revanced/com.google.android.youtube.apk; chmod 644 "$revancedapp" && chown system:system "$revancedapp" && chcon u:object_r:apk_data_file:s0 "$revancedapp"; mount -o bind "$revancedapp" "$stockapp" && am force-stop com.google.android.youtube && exit'
             then
                 echo "Mounting successful"
