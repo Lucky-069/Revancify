25bdd1346b03eb1ea6ec8775647f186632876fdb
decipher3114 <shauryasiwach26@gmail.com>
2022-09-28 15:52:10 +0530

    fallback to default, added patch selector

--- getver.sh
+++ getver.sh
@@ -5,8 +5,8 @@ arch="arm64"
 
 appname=$(dialog --backtitle "Revancify" --title 'Select App' --no-items --ascii-lines --ok-label "Select" --menu "Select Option" 20 40 10 "YouTube" "YouTubeMusic" "Twitter" "Reddit" "TikTok" 2>&1> /dev/tty)
 
-appver=($(python3 version-list.py "$appname"))
-linkfetch=$(dialog --backtitle "Revancify" --title $appname --no-items --ascii-lines --ok-label "Select" --menu "Select App Version" 20 40 10 "${appver[@]}" 2>&1> /dev/tty)
+appverlist=($(python3 version-list.py "$appname"))
+appver=$(dialog --backtitle "Revancify" --title $appname --no-items --ascii-lines --ok-label "Select" --menu "Select App Version" 20 40 10 "${appver[@]}" 2>&1> /dev/tty)
 
 if [ "$appname" = "" ]
 then
@@ -17,6 +17,6 @@ then
     print $appname
 fi
 
-link=$(python3 fetch-link.py "$appname" "$linkfetch")
+getlink=$(python3 fetch-link.py "$appname" "$appver")
 clear
 echo $link
