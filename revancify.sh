febec9ef604945ab0e3d59c1717ee14259071510
decipher3114 <shauryasiwach26@gmail.com>
2022-10-02 16:04:48 +0530

    edited something

--- revancify.sh
+++ revancify.sh
@@ -443,7 +443,7 @@ then
     then
         echo "Mounting the app"
 
-        if su -mm -c 'pkgname=$( ls /data/data/com.termux/files/home/storage | grep com.google | sed 's/.apk//g' ) && stockapp=$(pm path "$pkgname" | grep base | sed 's/package://g'); grep "$pkgname" /proc/mounts | while read -r line; do echo $line | cut -d " " -f 2 | xargs -r umount -l > /dev/null 2>&1; done; rm /data/adb/revanced/"$pkgname".apk > /dev/null 2>&1; mv "$pkgname".apk /data/adb/revanced && revancedapp=/data/adb/revanced/"$pkgname".apk; chmod 644 "$revancedapp" && chown system:system "$revancedapp" && chcon u:object_r:apk_data_file:s0 "$revancedapp"; mount -o bind "$revancedapp" "$stockapp" && am force-stop com.google.android.apps.youtube.music && exit'
+        if su -mm -c 'pkgname=$( ls /data/data/com.termux/files/home/storage/Revancify | grep com.google.android | sed 's/.apk//g' ) && stockapp=$(pm path "$pkgname" | grep base | sed 's/package://g') && grep "$pkgname" /proc/mounts | while read -r line; do echo $line | cut -d " " -f 2 | xargs -r umount -l > /dev/null 2>&1; done && rm /data/adb/revanced/"$pkgname".apk > /dev/null 2>&1 && mv "$pkgname".apk /data/adb/revanced && revancedapp=/data/adb/revanced/"$pkgname".apk && chmod 644 "$revancedapp" && chown system:system "$revancedapp" && chcon u:object_r:apk_data_file:s0 "$revancedapp"; mount -o bind "$revancedapp" "$stockapp" && am force-stop com.google.android.apps.youtube.music && exit'
         then
             echo "Mounting successful"
             tput cnorm && cd ~ && exit
