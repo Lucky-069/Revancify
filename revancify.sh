353b51c100f7b056440170450370c765826984e7
decipher3114 <shauryasiwach26@gmail.com>
2022-10-02 13:27:18 +0530

    fixed patch saving

--- revancify.sh
+++ revancify.sh
@@ -196,52 +196,6 @@ fi
 grep -q decipher ~/.dialogrc || cp dialog-config.txt ~/.dialogrc
 grep -q flag ~/../usr/bin/revancify || cp revancify ~/../usr/bin/revancify
 
-anim()
-{
-    echo "Fetching latest version info"
-    echo ""
-    echo "Please Wait..."
-    echo ""
-    tput sc
-    echo "█░░░░░░░░░"
-    sleep 0.1s
-    tput rc
-    echo "░█░░░░░░░░"
-    sleep 0.1s
-    tput rc
-    echo "░░█░░░░░░░"
-    sleep 0.1s
-    tput rc
-    echo "░░░█░░░░░░"
-    sleep 0.1s
-    tput rc
-    echo "░░░░█░░░░░"
-    sleep 0.1s
-    tput rc
-    echo "░░░░░█░░░░"
-    sleep 0.1s
-    tput rc
-    echo "░░░░░░█░░░"
-    sleep 0.1s
-    tput rc
-    echo "░░░░░░░█░░"
-    sleep 0.1s
-    tput rc
-    echo "░░░░░░░░█░"
-    sleep 0.1s
-    tput rc
-    echo "░░░░░░░░░█"
-    sleep 0.1s
-    tput rc
-    echo "░░░░░░░░░░"
-    sleep 0.1s
-    tput rc
-    tput cuu1
-    tput cuu1
-    tput cuu1
-    tput cuu1
-    tput sc
-}
 
 selectapp()
 {
@@ -281,15 +235,13 @@ selectpatches()
     done < <(jq -r --arg pkgname "$pkgname" 'map(select(.appname == $pkgname))[] | "\(.patchname) \(.status)"' patches.json)
     mapfile -t choices < <(dialog --backtitle "Revancify" --title 'Patch Selection Menu' --no-items --ascii-lines --ok-label "Save" --no-cancel --separate-output --checklist "Select patches to include" 20 45 10 "${patches[@]}" 2>&1 >/dev/tty)
     tmp=$(mktemp)
-    jq --arg pkgname "$pkgname" 'map(select(.appname == $pkgname).status = "off")' patches.json | jq 'map(select(IN(.patchname; $ARGS.positional[])).status = "on")' --args "${choices[@]}" > "$tmp" && mv "$tmp" patches.json
+    jq --arg pkgname "$pkgname" 'map(select(.appname == $pkgname).status = "off")' patches.json | jq 'map(select(IN(.patchname; $ARGS.positional[])).status = "on")' --args "${choices[@]}" > "$tmp" && mv "$tmp" ./patches.json
     mainmenu
 }
 
 
 patchoptions()
 {
-    echo ""
-    echo "Generating Options File. Please wait..."
     java -jar ./revanced-cli*.jar -b ./revanced-patches*.jar -m ./revanced-integrations*.apk -c -a ./noinput.apk -o nooutput.apk > /dev/null 2>&1
     tput cnorm
     tmp=$(mktemp)
