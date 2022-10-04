0466fa456a2c5b09ad51430454eae00754d8465d
decipher3114 <shauryasiwach26@gmail.com>
2022-10-04 13:06:50 +0530

    redirect to dev/null

--- revancify.sh
+++ revancify.sh
@@ -126,7 +126,7 @@ get_components(){
 }
 
 intro
-if ls ./revanced-patches* && ls ./revanced-cli* && ls ./revanced-integrations* > /dev/null 2>&1
+if ls ./revanced-patches* > /dev/null 2>&1 && ls ./revanced-cli* > /dev/null 2>&1 && ls ./revanced-integrations* > /dev/null 2>&1
 then
     :
 else
